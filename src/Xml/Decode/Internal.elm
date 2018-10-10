844ea44bbb2fc9cac14a53711951adffa969ff16
Matthew Dupree <mattjdupree@gmail.com>
2018-10-10 18:59:20 +0200

    Minor renaming and restructuring.

--- src/Xml/Decode/Internal.elm
+++ src/Xml/Decode/Internal.elm
@@ -1,17 +1,23 @@
-module Xml.Decode.Advanced exposing (..)
+module Xml.Decode.Internal exposing (SubTagDict, XmlContentValue(..), XmlValue, discardAttributeList, insertSubTag, mySpaces, parseXmlHelp, possibleComments, simpleContent, subTagContent, subTagContentHelp, xml, xmlFile, xmlTag)
 
-import Parser exposing (Parser,(|.),(|=),spaces,succeed,symbol,keyword, andThen)
 import Dict exposing (Dict)
+import Parser exposing ((|.), (|=), Parser, andThen, keyword, spaces, succeed, symbol)
 import Set exposing (Set)
 
-type alias XmlValue = (String,XmlContentValue)
+
+type alias XmlValue =
+    ( String, XmlContentValue )
+
 
 type XmlContentValue
     = SubTags SubTagDict
     | PresenceTag
     | XmlString String
 
-type alias SubTagDict = Dict String (List XmlContentValue)
+
+type alias SubTagDict =
+    Dict String (List XmlContentValue)
+
 
 xmlFile : Parser XmlValue
 xmlFile =
@@ -20,9 +26,10 @@ xmlFile =
         |. mySpaces
         |. possibleComments
         |. mySpaces
-        |= (xml)
+        |= xml
         |. mySpaces
         |. Parser.end
+
 
 possibleComments : Parser ()
 possibleComments =
@@ -30,80 +37,92 @@ possibleComments =
         |. Parser.chompUntil "-->"
         |. Parser.symbol "-->"
 
+
 xml : Parser XmlValue
 xml =
-    xmlTag 
-        |> andThen 
-            (\tag -> Parser.oneOf
-                [ succeed (tag,PresenceTag)
-                    |. symbol "/>"
-                , succeed identity
-                    |. symbol ">"
-                    |= parseXmlHelp tag
-                ]
+    xmlTag
+        |> andThen
+            (\tag ->
+                Parser.oneOf
+                    [ succeed ( tag, PresenceTag )
+                        |. symbol "/>"
+                    , succeed identity
+                        |. symbol ">"
+                        |= parseXmlHelp tag
+                    ]
             )
+
+
 
 {--| Detect a valid xml tag opener followed by a valid xml tag name, then discard the xml attribute list.
 
 Does not consume the tag close, as that may help define whether the tag has any content.
 
 See XML Naming Rules on https://www.w3schools.com/xml/xml_elements.asp to help understand valid tag names.--}
+
+
 xmlTag : Parser String
-xmlTag = 
+xmlTag =
     succeed identity
         |. symbol "<"
         |= Parser.variable
-            { start = (\c -> Char.isAlpha c || c == '_')
-            , inner = (\c -> Char.isAlphaNum c || Set.member c (Set.fromList ['-','_','.']))
+            { start = \c -> Char.isAlpha c || c == '_'
+            , inner = \c -> Char.isAlphaNum c || Set.member c (Set.fromList [ '-', '_', '.' ])
             , reserved = Set.singleton "xml"
             }
         |. discardAttributeList
 
+
 parseXmlHelp : String -> Parser XmlValue
 parseXmlHelp tag =
     Parser.oneOf
-        [ succeed (tag,PresenceTag)
+        [ succeed ( tag, PresenceTag )
             |. Parser.backtrackable mySpaces
-            |. symbol ("</"++tag++">")
-        , succeed (\x -> (tag,x))
+            |. symbol ("</" ++ tag ++ ">")
+        , succeed (\x -> ( tag, x ))
             |. Parser.backtrackable mySpaces
             |= subTagContent tag
-        , succeed (\x -> (tag,x))
+        , succeed (\x -> ( tag, x ))
             |= simpleContent
-            |. symbol ("</"++tag++">")
-        , Parser.problem 
+            |. symbol ("</" ++ tag ++ ">")
+        , Parser.problem
             "This super simple XML library doesn't support semi-structured XML. Please decide between either child tags or text inside any particular tag, not both!"
         ]
+
 
 simpleContent : Parser XmlContentValue
 simpleContent =
     Parser.getChompedString (Parser.chompUntil "</")
         |> Parser.map XmlString
 
+
 subTagContent : String -> Parser XmlContentValue
 subTagContent tag =
-    Parser.loop Dict.empty 
+    Parser.loop Dict.empty
         (subTagContentHelp tag)
 
+
 subTagContentHelp : String -> SubTagDict -> Parser (Parser.Step SubTagDict XmlContentValue)
-subTagContentHelp tag dict = 
+subTagContentHelp tag dict =
     Parser.oneOf
         [ succeed (Parser.Loop dict)
             |. possibleComments
         , succeed (Parser.Done (SubTags dict))
-            |. symbol ("</"++tag++">")
-        , succeed (\(t,x) -> Parser.Loop (dict |> insertSubTag t x))
+            |. symbol ("</" ++ tag ++ ">")
+        , succeed (\( t, x ) -> Parser.Loop (dict |> insertSubTag t x))
             |= xml
         ]
         |. mySpaces
 
+
 insertSubTag : String -> XmlContentValue -> SubTagDict -> SubTagDict
 insertSubTag tag xmltag =
-    Dict.update tag 
+    Dict.update tag
         (\m ->
             case m of
                 Just l ->
                     Just (xmltag :: l)
+
                 Nothing ->
                     Just (List.singleton xmltag)
         )
@@ -111,32 +130,35 @@ insertSubTag tag xmltag =
 
 discardAttributeList : Parser ()
 discardAttributeList =
-    (Parser.loop () <| always
-        ( succeed identity 
-            |. spaces 
-            |= Parser.oneOf
-                [ succeed (Parser.Loop ())
-                    |. Parser.variable
-                        { start = Char.isAlpha
-                        , inner = \c -> Char.isAlphaNum c || c == ':'
-                        , reserved = Set.empty
-                        }
-                    |. Parser.oneOf
-                        [ symbol "=\""
-                            |. Parser.chompUntil "\""
-                            |. symbol "\""
-                        , symbol "='"
-                            |. Parser.chompUntil "'"
-                            |. symbol "'"
-                        ]
-                , succeed (Parser.Done ())
-                ]
-        )
-    ) |. spaces
+    (Parser.loop () <|
+        always
+            (succeed identity
+                |. spaces
+                |= Parser.oneOf
+                    [ succeed (Parser.Loop ())
+                        |. Parser.variable
+                            { start = Char.isAlpha
+                            , inner = \c -> Char.isAlphaNum c || c == ':'
+                            , reserved = Set.empty
+                            }
+                        |. Parser.oneOf
+                            [ symbol "=\""
+                                |. Parser.chompUntil "\""
+                                |. symbol "\""
+                            , symbol "='"
+                                |. Parser.chompUntil "'"
+                                |. symbol "'"
+                            ]
+                    , succeed (Parser.Done ())
+                    ]
+            )
+    )
+        |. spaces
+
 
 mySpaces : Parser ()
 mySpaces =
     Parser.chompWhile
         (\c ->
-            Set.member c <| Set.fromList [' ','\n','\r','\t']
+            Set.member c <| Set.fromList [ ' ', '\n', '\u{000D}', '\t' ]
         )
